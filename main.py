8c6bfbf4d712745edb2916a93ea695908e358627
qwinox <60475943+qwinox@users.noreply.github.com>
2024-02-21 15:21:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -74,12 +74,12 @@ df_0 = df_0.round(6)
 # Трехлинейная диаграмма деформирования бетона на растяжение
 
 temp_1 = Rbt * 0.6 / Eb
-if temp_1 < 0.0001:
-    temp_1 = str(temp_1)
-    temp_1 = temp_1[:3] + temp_1[-4:] 
+# if temp_1 < 0.0001:
+#     temp_1 = str(temp_1)
+#     temp_1 = temp_1[:3] + temp_1[-4:] 
 data_1 = [
     (0, 0),
-    (Rbt * 0.6, temp_1),
+    (Rbt * 0.6, str(temp_1)),
     (Rbtn, 0.0001),
     (Rbtn, 0.00015)
 ]
